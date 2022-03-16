0b474eeb07b2659aa4b2446af4e247cd38b44553
esylva <82966354+esylva@users.noreply.github.com>
2022-03-16 15:02:28 +0300

    Update checker.c

--- srcs/parcer/checker.c
+++ srcs/parcer/checker.c
@@ -12,24 +12,23 @@
 
 #include "cub.h"
 
+// функция может вернуть -1!
 int	ft_name_check(char *carta)
 {
 	char	*point;
 	int	fd;
 	int	error;
 
+	error = 0;
 	point = ft_strrchr(carta, '.');
-	if (!point || ft_strcmp(point, ".cub"))
-	{
-		printf("%s\n", strerror(EINVAL));
-		exit(EINVAL);
-	}
-  	fd = open(carta, O_RDONLY);
-	error = errno;
+	fd = open(carta, O_RDONLY);
 	if (fd == -1)
-	{
+		error = errno;
+	else if (!point || ft_strcmp(point, ".cub"))
+		error = EINVAL;
+	else if(read(fd, NULL, 0) == -1)
+		error = errno;
+	if (error)
 		printf("%s\n", strerror(error));
-		exit(error);
-	}
 	return (fd);
 }

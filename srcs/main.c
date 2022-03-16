a541a1bb12c5fe89707df8a65b35e49f1241db77
esylva <82966354+esylva@users.noreply.github.com>
2022-03-16 15:21:43 +0300

    Update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -15,6 +15,7 @@
 int	main(int argc, char **argv)
 {
 	int		i;
+	int		ret;
 	t_data	*data;
 
 	data = NULL;
@@ -29,25 +30,33 @@ int	main(int argc, char **argv)
 		i = 1;
 		while (i < argc)
 		{
-			ft_cube(argv[i], data);
+			ret = ft_cube(argv[i], data)
+			if(ret)
+				return (ret);
 			i++;
 		}
 	}
 	return(0);
 }
 
-void	ft_cube(char *carta, t_data *data)
+int	ft_cube(char *carta, t_data *data)
 {
 	int	fd;
 
 	fd = ft_name_check(carta);
+	if (!fd)
+		return (1);
 	data = ft_map_init(data);
-	ft_parce_map(data, fd);
+	if (!data)
+		return (2);
+	if (!ft_parce_map(data, fd))
+		return (3);
 	ft_make_coords(data);
 	ft_make_map(data, fd);
 	ft_check_lines(data);
 	ft_win_init(data);
 	ft_free_all(data);
+	return (0);
 }
 
 void	ft_free_all(t_data *data)
